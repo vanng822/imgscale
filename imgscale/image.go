50747e881bdb517e68b1e2b5a6e83d6413366e0c
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-01-11 12:40:19 +0100

    Make it possible to configure ImageProviderHTTP so that it can fetch an arbitrary remote image. Should think twice before using it.

--- imgscale/image.go
+++ imgscale/image.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gographics/imagick/imagick"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 type ImageInfo struct {
@@ -88,13 +89,25 @@ func (imageProvider ImageProviderFile) Fetch(info *ImageInfo) (*imagick.MagickWa
 	return img, err
 }
 
+func NewImageProviderFile(path string) ImageProvider {
+	if path == "" {
+		panic("Path can not be empty")
+		// Should check if path readable here as well
+	}
+	provider := ImageProviderFile{}
+	provider.Path = strings.TrimSuffix(path, "/")
+	return provider
+}
+
 type ImageProviderHTTP struct {
 	BaseUrl string
 }
 
 func (imageProvider ImageProviderHTTP) Fetch(info *ImageInfo) (*imagick.MagickWand, error) {
 	img := imagick.NewMagickWand()
-	resp, err := http.Get(fmt.Sprintf("%s/%s", imageProvider.BaseUrl, info.Filename))
+	// %s%s will make it possible for using on arbitrary remote image
+	// like http://127.0.0.1:8081/img/100x100/http://127.0.0.1:8080/img/original/kth.jpg
+	resp, err := http.Get(fmt.Sprintf("%s%s", imageProvider.BaseUrl, info.Filename))
 	if err != nil {
 		return img, err
 	}
@@ -106,4 +119,13 @@ func (imageProvider ImageProviderHTTP) Fetch(info *ImageInfo) (*imagick.MagickWa
 	}
 	err = img.ReadImageBlob(imgData)
 	return img, err
+}
+
+func NewImageProviderHTTP(baseUrl string) ImageProvider {
+	provider := ImageProviderHTTP{}
+	if baseUrl != "" {
+		// assume we have a valid base url
+		provider.BaseUrl = fmt.Sprintf("%s/", strings.TrimSuffix(baseUrl, "/"))
+	}
+	return provider
 }
