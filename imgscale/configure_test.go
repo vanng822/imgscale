9fa47e5035093ffdae7b25c1d82516442fe70bee
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-01-13 19:25:07 +0100

    Adding tests for configure

---
+++ imgscale/configure_test.go
@@ -0,0 +1,38 @@
+package imgscale
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPanicExtNotSupported(t *testing.T) {
+	config := &Config{}
+	config.Exts = []string{"jpg", "bip"}
+	assert.Panics(t, func() {
+		configure(config)
+	})
+}
+
+func TestPanicDuplicateFormat(t *testing.T) {
+	config := &Config{}
+	config.Exts = []string{"jpg", "png"}
+	config.Formats = append(config.Formats, &Format{Prefix: "100x100", Height: 100, Ratio: 1.0, Thumbnail: true})
+	config.Formats = append(config.Formats, &Format{Prefix: "100x100", Height: 100, Ratio: 1.0, Thumbnail: true})
+	assert.Panics(t, func() {
+		configure(config)
+	})
+}
+
+func TestConfigureOK(t *testing.T) {
+	config := &Config{}
+	config.Path = "./"
+	config.Prefix = "img"
+	config.Exts = []string{"jpg", "png"}
+	config.Formats = append(config.Formats, &Format{Prefix: "100x100", Height: 100, Ratio: 1.0, Thumbnail: true})
+	config.Formats = append(config.Formats, &Format{Prefix: "original", Height: 0, Ratio: 0.0, Thumbnail: false})
+	handler := configure(config)
+	expected := "/img/(?P<format>100x100|original)/(?P<filename>.+)\\.(?P<ext>jpg|png)"
+	assert.Equal(t, handler.regexp.String(), expected) 
+	assert.Len(t, handler.formats, 2)
+	assert.Equal(t, handler.supportedExts, supportedExts)
+}
