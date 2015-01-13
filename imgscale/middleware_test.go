b4e4483c6413f8df34b95e434024d608d0c01331
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-01-13 20:26:59 +0100

    Adding test for middleware Configure

---
+++ imgscale/middleware_test.go
@@ -0,0 +1,11 @@
+package imgscale
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMiddlewareConfigure(t *testing.T) {
+	handler := Configure("../config/formats.json")
+	assert.NotNil(t, handler, nil)
+}
