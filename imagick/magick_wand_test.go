7babbb3836463a34ff93001ea5dee24834639b4a
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-01-19 19:33:34 +0100

    Adding test for ReadImageBlob and fix GetImageBlob using InEpsilon

--- imagick/magick_wand_test.go
+++ imagick/magick_wand_test.go
@@ -4,6 +4,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"path/filepath"
 	"testing"
+	"os"
+	"io/ioutil"
 )
 
 func TestReadImageWrongFile(t *testing.T) {
@@ -33,5 +35,22 @@ func TestGetImageBlob(t *testing.T) {
 	defer img.Destroy()
 	err := img.ReadImage("./test_data/kth.jpg")
 	assert.Nil(t, err)
-	assert.Equal(t, len(img.GetImageBlob()), 28611)
+	assert.InEpsilon(t, len(img.GetImageBlob()), 28611, 10)
+}
+
+
+func TestReadImageBlob(t *testing.T) {
+	img := NewMagickWand()
+	defer img.Destroy()
+	
+	file, err := os.Open("./test_data/kth.jpg")
+	assert.Nil(t, err)
+	defer file.Close()
+	imgData, err := ioutil.ReadAll(file)
+	assert.Nil(t, err)
+	err = img.ReadImageBlob(imgData)
+	assert.Nil(t, err)
+	assert.InEpsilon(t, len(img.GetImageBlob()), 28611, 10)
+	assert.Equal(t, int(img.GetImageWidth()), 320)
+	assert.Equal(t, int(img.GetImageHeight()), 240)
 }
