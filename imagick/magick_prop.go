3116548ebeecc4fb9f88e5c76a83d271bcde0432
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-01-20 20:44:09 +0100

    Set and Get ResourceLimit could be nice to have

--- imagick/magick_prop.go
+++ imagick/magick_prop.go
@@ -1,14 +1,25 @@
 package imagick
+
 /*
 #include <wand/MagickWand.h>
 */
 import "C"
 
-import (
-)
+import ()
 
 func GetVersion() (string, uint) {
 	cnversion := C.size_t(0)
 	csversion := C.MagickGetVersion(&cnversion)
 	return C.GoString(csversion), uint(cnversion)
+}
+
+func SetResourceLimit(resourceType ResourceType, limit int64) bool {
+	res := C.MagickSetResourceLimit(C.ResourceType(resourceType), C.MagickSizeType(limit))
+	return BOOLEAN_TYPE_TRUE == BooleanType(res)
+
+}
+
+// Returns the specified resource limit in megabytes.
+func GetResourceLimit(resourceType ResourceType) int64 {
+	return int64(C.MagickGetResourceLimit(C.ResourceType(resourceType)))
 }
--- imagick/magick_prop_test.go
+++ imagick/magick_prop_test.go
@@ -10,3 +10,10 @@ func TestGetVersion(t *testing.T) {
 	assert.Regexp(t, "ImageMagick", sversion)
 	assert.True(t, nversion > 0)
 }
+
+
+func TestResourceLimit(t *testing.T) {
+	area := GetResourceLimit(RESOURCE_AREA)/2
+	assert.True(t, SetResourceLimit(RESOURCE_AREA, area))
+	assert.Equal(t, area, GetResourceLimit(RESOURCE_AREA))
+}
