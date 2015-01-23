48c0e149c201617a8bd623433815bd8aefd7fba5
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-01-23 09:45:11 +0100

    Rewrite so we can build an binary for standalone server without any further dependencies. Need to learn more about go command. For now one has to build with o flag, go build -o bin/imgscale imgscale.go

--- imgscale.go
+++ imgscale.go
@@ -1,34 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/codegangsta/negroni"
-	"github.com/go-martini/martini"
 	"github.com/vanng822/imgscale/imgscale"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	n := negroni.New()
-	handler := imgscale.Configure("./config/formats.json")
+	var (
+		configPath string
+		host       string
+		port       int
+		pidFile    string
+		force      bool
+	)
+
+	flag.StringVar(&host, "h", "127.0.0.1", "Host to listen on")
+	flag.IntVar(&port, "p", 8080, "Port number to listen on")
+	flag.StringVar(&configPath, "c", "./config/formats.json", "Path to configurations")
+	flag.StringVar(&pidFile, "pid", "imgscale.pid", "Pid file")
+	flag.BoolVar(&force, "f", false, "Force and remove pid file")
+	flag.Parse()
+
+	if pidFile != "" {
+		if !force {
+			if _, err := os.Stat(pidFile); err == nil {
+				log.Printf("Could not create pid file, error: %v", err)
+				panic(fmt.Sprintf("Pidfile %s exist", pidFile))
+			}
+		}
+		pid := syscall.Getpid()
+		pidf, err := os.Create(pidFile)
+		if err != nil {
+			log.Printf("Could not create pid file, error: %v", err)
+			panic("Could not create pid file")
+		}
+		pidf.WriteString(fmt.Sprintf("%d", pid))
+		pidf.Close()
+		defer func() {
+			log.Println("Cleaning up pid file")
+			err := os.Remove(pidFile)
+			if err != nil {
+				log.Printf("Fail to clean up pid file %s", pidFile)
+			}
+		}()
+	}
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Kill, os.Interrupt, syscall.SIGTERM)
+
+	handler := imgscale.Configure(configPath)
 	defer handler.Cleanup()
-	// Example how to run an arbitrary remote image provider
-	handler.SetImageProvider(imgscale.NewImageProviderHTTP(""))
-	n.UseHandler(handler)
-	go http.ListenAndServe(fmt.Sprintf("%s:%d", "127.0.0.1", 8081), n)
+	http.Handle("/", handler)
+	log.Printf("listening to address %s:%d", host, port)
+	go http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil)
 
-	// Martini
-	app := martini.Classic()
-	mhandler := imgscale.Configure("./config/formats.json")
-	defer mhandler.Cleanup()
-	app.Use(mhandler.ServeHTTP)
-	go http.ListenAndServe(fmt.Sprintf("%s:%d", "127.0.0.1", 8080), app)
-
-	// http.Handler
-	handler2 := imgscale.Configure("./config/formats.json")
-	defer handler2.Cleanup()
-	// Example how to run an host limited remote image provider, can not run arbitrary here
-	handler2.SetImageProvider(imgscale.NewImageProviderHTTP("http://127.0.0.1:8080/img/original/"))
-	http.Handle("/", handler2)
-	http.ListenAndServe(fmt.Sprintf("%s:%d", "127.0.0.1", 8082), nil)
+	sig := <-sigc
+	log.Printf("Got signal: %s", sig)
 }
