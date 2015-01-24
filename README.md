7a49a816b0ad30a9fa708341009a19eda23464bf
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-01-24 12:38:02 +0100

    Some improvement on pid file handling

--- README.md
+++ README.md
@@ -29,6 +29,10 @@ You need to install ImageMagick first.
 
 	go get github.com/vanng822/imgscale/imagick
 
+### gopid
+	
+	go get github.com/vanng822/gopid
+	
 ## Install 
 
 	go get github.com/vanng822/imgscale/imgscale
--- imgscale.go
+++ imgscale.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/vanng822/gopid"
 	"github.com/vanng822/imgscale/imgscale"
 	"log"
 	"net/http"
@@ -27,29 +28,12 @@ func main() {
 	flag.BoolVar(&force, "f", false, "Force and remove pid file")
 	flag.Parse()
 
-	if pidFile != "" {
-		if !force {
-			if _, err := os.Stat(pidFile); err == nil {
-				log.Printf("Could not create pid file, error: %v", err)
-				panic(fmt.Sprintf("Pidfile %s exist", pidFile))
-			}
-		}
-		pid := syscall.Getpid()
-		pidf, err := os.Create(pidFile)
-		if err != nil {
-			log.Printf("Could not create pid file, error: %v", err)
-			panic("Could not create pid file")
-		}
-		pidf.WriteString(fmt.Sprintf("%d", pid))
-		pidf.Close()
-		defer func() {
-			log.Println("Cleaning up pid file")
-			err := os.Remove(pidFile)
-			if err != nil {
-				log.Printf("Fail to clean up pid file %s", pidFile)
-			}
-		}()
+	if pidFile != "" {		
+		gopid.CheckPid(pidFile, force)
+		gopid.CreatePid(pidFile)
+		defer gopid.CleanPid(pidFile)
 	}
+
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, os.Kill, os.Interrupt, syscall.SIGTERM)
 
