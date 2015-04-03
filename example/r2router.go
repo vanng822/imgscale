68bea52dcee467230ae3a4b3e57088928634518f
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-04-03 12:10:34 +0200

    R2router example

---
+++ example/r2router.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vanng822/imgscale/imgscale"
+	"github.com/vanng822/r2router"
+	"net/http"
+)
+
+func main() {
+	app := r2router.NewSeeforRouter()
+	mhandler := imgscale.Configure("./config/formats.json")
+	defer mhandler.Cleanup()
+	app.Before(r2router.BeforeFunc(func(w http.ResponseWriter, req *http.Request, next func()) {
+		mhandler.ServeHTTP(w, req)
+	}))
+	http.ListenAndServe(fmt.Sprintf("%s:%d", "127.0.0.1", 8080), app)
+}
